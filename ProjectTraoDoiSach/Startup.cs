df8d9cd5bc2811f6de8471f78af883084bf4829f
Quy Le <quyle1703@gmail.com>
2017-05-15 15:03:06 +0700

    Project Trao Doi Sach Bang .NET

---
+++ ProjectTraoDoiSach/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectTraoDoiSach.Startup))]
+namespace ProjectTraoDoiSach
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
